335513b27d48586d2a5301fc9727619ab8c1e0fc
MichaelH13 <mhiggins13@georgefox.edu>
2016-02-11 13:22:29 -0800

    Running commands with args now

--- shell.c
+++ shell.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#define MAX_SIZE 256
+
+#define MAX_SIZE 4096
 
 int main(int argc, char* argv[])
 {
@@ -10,16 +11,22 @@ int main(int argc, char* argv[])
     //
     /* Variable declarations */
     char command[MAX_SIZE];
-    char* commandArgs =  malloc(256); /* Max number */
+    char* commandArgs[MAX_SIZE]; /* Max number */
     int childPid = 0;
+    int blnRunBackground = 0;
+    int blnIsCwd = 0;
+    int blnIsCd = 0;
     char* foo;
+    char* returnPointer;
+    int i = 0;
     
     while(1) /* Repeat forever */ {
+        i = 0;
         /* Display a prompt */
         printf("myShell%% ");
 
         /* Read a command from the user */
-        fgets(command,sizeof(command), stdin);   
+        fgets(command, MAX_SIZE, stdin);
 
         /* Parse the string into the command portion and
          * an array of pointers to argument strings using the blank
@@ -28,27 +35,51 @@ int main(int argc, char* argv[])
          * the command and the array must have a NULL pointer
          * after the last argument.
          */
-        //puts(command);
 
-        /* If the user entered 'exit' then call the exit() system call
-         * to terminate the process
-         */
         foo = strtok(command, " ");
         
         while (foo) {
-            strcat(commandArgs, foo);
-            strcat(commandArgs, " ");
-            puts(commandArgs);
+            
+            // If the last character in the string is a return, throw it away.
+            commandArgs[i] = foo;
+            
+            if ((returnPointer = strchr(commandArgs[i++], '\n')) != NULL)
+            {
+                *returnPointer = '\0';
+            }
+            
+            // If the current string is an '&' then we need to print the
+            // job ID so set our flag.
+//            if (strcmp(foo, "&") == 0) {
+//                blnRunBackground = -1;
+//            }
+//            else
+//            {
+//                strcat(**commandArgs, foo);
+//            }
+            
+//            if (strcmp(foo, "cd") == 0) {
+//                blnIsCd = -1;
+//            }
+//            if (strcmp(foo, cwd) == 0) {
+//                blnIsCwd = -1;
+//            }
+            
             foo = strtok(NULL, " ");
         }
+        
+        /* If the user entered 'exit' then call the exit() system call
+         * to terminate the process
+         */
 
         /* Fork a child process to execute the command and return 
          * the result of the fork() in the childPid variable so 
          * we know whether we're now executing as the parent 
          * or child and whether or not the fork() succeeded
          */
-        childPid = fork();
-
+        if (!(blnIsCwd || blnIsCwd)) {
+            childPid = fork();
+        }
 
         if (!childPid) /* We forked no child, we ARE the child */ {
             /* We're now executing in the context of the child process.
@@ -56,9 +87,20 @@ int main(int argc, char* argv[])
              * the process' memory with the executable command the 
              * user has asked for.  
              */
-            printf("command: %s\ncommandArgs: %s", command, commandArgs);
-            system(commandArgs);
-            //            return execv(command, &commandArgs);
+            
+//            commandArgs[2] = NULL;
+//            puts("commandArgs[0]:");
+//            puts(commandArgs[0]);
+//            puts("commandArgs[1]:");
+//            puts(commandArgs[1]);
+//            puts("commandArgs[2]:");
+//            puts(commandArgs[2]);
+            
+//            execvp(cmd, args);
+            execvp(*commandArgs, commandArgs);
+            
+            return 0;
+//            return execlp(command, commandArgs);
         }
         else if (childPid < 0) {
             /* An error occured during the fork - print it */
@@ -70,11 +112,20 @@ int main(int argc, char* argv[])
              * Wait for the child to finish before we prompt
              * again.
              */
-            wait(NULL);
+            if (blnRunBackground) {
+                printf("Job %d\n", childPid);
+            }
+            else if (blnIsCd) {
+                //call our cd command
+            }
+            else {
+                wait(NULL);
+            }
         }
         
         // Reset the command args pointer.
-        *commandArgs = '\0';
+        blnRunBackground = 0;
+        blnIsCwd = 0;
 
     } /* while */
 
